e0a26addfeee7348353c4b90b79e9ecd2d6f7fd3
CarlosDanielBernal <carlos2220576@gmail.com>
2020-09-03 21:35:07 -0500

    ejercicios

---
+++ Ejercicio_funcional.js
@@ -0,0 +1,12 @@
+/* const multiplicar_y_sumar = [2,4,6,7,9]
+const resultado =multiplicar_y_sumar.map(m=> m*3-2);
+
+console.log(resultado) */
+
+//valor de envio con iva
+const valor_envio =[20000,22000,23000,26000]
+
+
+const  totalenvio =valor_envio.map(e=> e*0.20+e)
+console.log(totalenvio)
+
---
+++ Ejercicio_recreativa.js
@@ -0,0 +1,6 @@
+function   helado() {
+    console.log("mi helado favorito es el helado de vainilla")
+    helado ();
+    
+}
+    helado();
